97527229b0450736918262bb967a5dcb251925ec
ilyagladky1981 <ilyagladky1981@gmail.com>
2023-11-13 12:23:52 +0300

    Server and Clirnt v1.0

--- SecureRDPServer.cpp
+++ SecureRDPServer.cpp
@@ -12,7 +12,93 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	WSADATA wsData;
+
+	int erStat = WSAStartup(MAKEWORD(2, 2), &wsData);
+
+	if (erStat != 0) {
+		cout << "Error WinSock version initializaion #";
+		cout << WSAGetLastError();
+		return 1;
+	}
+	else
+		cout << "WinSock initialization is OK" << endl;
+
+	SOCKET ServSock = socket(AF_INET, SOCK_STREAM, 0);
+
+
+
+	if (ServSock == INVALID_SOCKET) {
+		cout << "Error initialization socket # " << WSAGetLastError() << endl;
+		closesocket(ServSock);
+		WSACleanup();
+		return 1;
+	}
+	else
+		cout << "Server socket initialization is OK" << endl;
+
+	in_addr ip_to_num;
+	erStat = inet_pton(AF_INET, “127.0.0.1”, &ip_to_num);
+	if (erStat <= 0) {
+		cout << "Error in IP translation to special numeric format" << endl;
+		return 1;
+	}
+
+
+
+	sockaddr_in servInfo;
+	ZeroMemory(&servInfo, sizeof(servInfo));
+
+	servInfo.sin_family = AF_INET;
+	servInfo.sin_addr = ip_to_num;
+	servInfo.sin_port = htons(1234);
+
+	erStat = bind(ServSock, (sockaddr*)&servInfo, sizeof(servInfo));
+	if (erStat != 0) {
+		cout << "Error Socket binding to server info. Error # " << WSAGetLastError() << endl;
+		closesocket(ServSock);
+		WSACleanup();
+		return 1;
+	}
+	else
+		cout << "Binding socket to Server info is OK" << endl;
+
+
+	erStat = listen(ServSock, SOMAXCONN);
+
+	if (erStat != 0) {
+		cout << "Can't start to listen to. Error # " << WSAGetLastError() << endl;
+		closesocket(ServSock);
+		WSACleanup();
+		return 1;
+	}
+	else {
+		cout << "Listening..." << endl;
+	}
+
+
+
+	sockaddr_in clientInfo;
+
+	ZeroMemory(&clientInfo, sizeof(clientInfo));
+
+	int clientInfo_size = sizeof(clientInfo);
+
+	SOCKET ClientConn = accept(ServSock, (sockaddr*)&clientInfo, &clientInfo_size);
+
+	if (ClientConn == INVALID_SOCKET) {
+		cout << "Client detected, but can't connect to a client. Error # " << WSAGetLastError() << endl;
+		closesocket(ServSock);
+		closesocket(ClientConn);
+		WSACleanup();
+		return 1;
+	}
+	else
+		cout << "Connection to a client established successfully" << endl;
+
+
+
+
 
 }
 
